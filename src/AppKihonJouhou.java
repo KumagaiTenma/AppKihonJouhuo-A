b0377f78b79b5dbb48f9d21027b978b71b4a2c06
Your Name <tenma.kumagai.sys@gmail.com>
2017-07-04 11:59:46 +0900

    first commit

---
+++ src/AppKihonJouhou.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class AppKihonJouhou {
+	int point;
+	int total;
+	Scanner sc = new Scanner(System.in);
+	public static void main(String[] args){
+
+	}
+	public int question1(){
+		    return point;
+    }
+	public int question2(){
+		    return point;
+	}
+	public int question3(){
+		    return point;
+	}
+	public int question4(){
+		    return point;
+	}
+	public int question5(){
+		    return point;
+	}
+	public int question6(){
+		    return point;
+	}
+
+}
